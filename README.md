96a5a6bb0884dcff0ccf82a95fbca9c482b1908f
Sebastian 'gonX' Jensen <gonx@gonx.dk>
2023-10-27 00:06:05 +0200

    Initial theme and asset copy

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # opentabletdriver.github.io
 The next version of OpenTabletDriver.Web
+
+# Local Development
+
+## Requirements
+
+- `rubygems`
+
+## Steps
+
+```bash
+$ gem install jekyll bundler
+$ cd <website root> # (currently git root)
+$ bundle install
+$ bundle exec jekyll serve --livereload
+```
---
+++ assets/js/site.js
@@ -0,0 +1,26 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+function ajaxPost(url, data, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4) {
+            callback(xhr);
+        }
+    };
+    xhr.open('POST', url);
+    xhr.send(data);
+}
+
+function ajaxGet(url, data, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4) {
+            callback(xhr);
+        }
+    };
+    xhr.open('GET', url);
+    xhr.send(data);
+}

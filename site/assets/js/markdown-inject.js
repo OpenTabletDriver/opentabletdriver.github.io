8e5bb32ecbe94892243c2cedede3db328edfc099
X9VoiD <oscar.silvestrexx@gmail.com>
2023-12-08 12:21:09 +0800

    styling: Use bootstrap alerts to style blockquotes

--- site/assets/js/markdown-inject.js
+++ site/assets/js/markdown-inject.js
@@ -1,29 +1,50 @@
 function injectMarkdownClasses() {
-    // Select the parent element with class markdown-content
-    var parentElement = document.querySelector('.markdown-content');
-    if (parentElement === null)
-        return;
+  let parentElement = document.querySelector('.markdown-content');
+  if (parentElement === null)
+    return;
 
-    // From the selected parent, select all direct child blockquote elements
-    var blockquoteElements = parentElement.querySelectorAll('blockquote');
-    if (blockquoteElements === null)
-        return;
-
-    // Loop through each blockquote element and add the desired classes
-    blockquoteElements.forEach(function (blockquoteElement) {
-        blockquoteElement.classList.add('small');
-        blockquoteElement.classList.add('text-muted');
+  let injectMarkdownClassConditional = (selector, condition, classes) => {
+    parentElement.querySelectorAll(selector)?.forEach((element) => {
+      if (condition(element)) {
+        classes.forEach((classToAdd) => {
+          element.classList.add(classToAdd);
+        });
+      }
     });
+  }
 
-    // Select all table elements
-    var tableElements = parentElement.querySelectorAll('table');
-    if (tableElements === null)
-        return;
+  let injectMarkdownClass = (selector, classes) => {
+    injectMarkdownClassConditional(selector, (element) => true, classes);
+  }
 
-    // Loop through each table element and add the desired classes
-    tableElements.forEach(function (tableElement) {
-        tableElement.classList.add('table');
-    });
+  let alertTypes = [
+    'alert-success',
+    'alert-info',
+    'alert-warning',
+    'alert-danger',
+    'alert-primary',
+    'alert-secondary',
+    'alert-light',
+    'alert-dark'
+  ];
+
+  injectMarkdownClass('blockquote', ['alert']);
+  injectMarkdownClassConditional('blockquote', hasNoClass(alertTypes), ['alert-secondary']);
+  injectMarkdownClass('& > * blockquote', ['alert-compact', 'small']); // blockquotes that are not an immediate children
+  injectMarkdownClass('blockquote a', ['alert-link']);
+  injectMarkdownClass('table', ['table']);
+}
+
+function hasNoClass(classes) {
+  return (element) => {
+    for (classToFind of classes) {
+      if (element.classList.contains(classToFind)) {
+        return false
+      }
+    }
+
+    return true;
+  }
 }
 
 injectMarkdownClasses();
